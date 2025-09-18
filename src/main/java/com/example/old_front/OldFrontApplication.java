f241d06d760fe5963aa403cc88ccc496431852f2
kjy <rlawhdduq95@gmail.com>
2025-09-18 21:21:44 +0900

    최초 업로드

---
+++ src/main/java/com/example/old_front/OldFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.old_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/old_front/OldFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.old_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
