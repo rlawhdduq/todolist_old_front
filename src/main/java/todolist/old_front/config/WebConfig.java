3458d909cb80af6a538416e3deff6b7bf88b3bda
kjy <rlawhdduq95@gmail.com>
2025-10-05 19:11:45 +0900

    interceptor 수정

--- src/main/java/todolist/old_front/config/WebConfig.java
+++ src/main/java/todolist/old_front/config/WebConfig.java
@@ -17,24 +17,25 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addInterceptors(InterceptorRegistry registry)
     {
-        // List<String> excludeUrlApi = Arrays.asList("/api/v1/auth", 
-        //                                                 "/api/v1/user/login", 
-        //                                                 "/api/v1/user/join");
-        // List<String> excludeUrlController = Arrays.asList("/user/login",
-        //                                                         "/user/join");
-        // List<String> excludeUrlCommon = Arrays.asList("/",
-        //                                                     "/css/**", 
-        //                                                     "/error", 
-        //                                                     "/*.ico");
-
-        // excludeUrlApi.addAll(excludeUrlController);
-        // excludeUrlApi.addAll(excludeUrlCommon);
-
-        // List<String> excludeUrl = excludeUrlApi;
+        List<String> excludeUrl = Arrays.asList(
+                                    // Api
+                                "/api/v1/auth", 
+                                    "/api/v1/user/login", 
+                                    "/api/v1/user/join",
+                                    // Controller(View)
+                                    "/",
+                                    "/user/login",
+                                    "/user/join",
+                                    // Css/Js
+                                    "/js/**",
+                                    "/css/**",
+                                    "/error",
+                                    "/*.ico"
+                                    );
         
-        // registry.addInterceptor(new LoginCheckInterceptor())    // 1. 인터셉터 등록
-        //         .order(1)                                 // 2. 인터셉터 체인 순서 지정
-        //         .addPathPatterns("/**")             // 3. 전체에 적용시키고
-        //         .excludePathPatterns(excludeUrl);               // 4. 인터셉터에서 제외할 패턴 지정
+        registry.addInterceptor(new LoginCheckInterceptor())    // 1. 인터셉터 등록
+                .order(1)                                 // 2. 인터셉터 체인 순서 지정
+                .addPathPatterns("/**")             // 3. 전체에 적용시키고
+                .excludePathPatterns(excludeUrl);               // 4. 인터셉터에서 제외할 패턴 지정
     }
 }
