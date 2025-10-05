53b9f4589a61117cbad7431e0ffe33df2081cc90
kjy <rlawhdduq95@gmail.com>
2025-10-05 23:01:43 +0900

    folow(get)의 url을 쿼리파라미터가 아닌 {}형태로 변경

--- src/main/java/todolist/old_front/service/follow/impl/FollowServiceImpl.java
+++ src/main/java/todolist/old_front/service/follow/impl/FollowServiceImpl.java
@@ -57,8 +57,7 @@ public class FollowServiceImpl implements FollowService{
     {
         Map<String, List<Long>> followerList = webClient.get()
                                             .uri(
-                                                uriBuilder -> uriBuilder.path(gatewayUrl+"/api/v1/follow")
-                                                                        .queryParam("userId", userId)
+                                                uriBuilder -> uriBuilder.path(String.format(gatewayUrl+"/api/v1/follow/{userId}", userId))
                                                                         .build()
                                                 )
                                             .retrieve()
