02d85a9ac719fad4693a20e8d8858d279cc0db62
kjy <rlawhdduq95@gmail.com>
2025-10-05 11:48:26 +0900

    interceptor 수정(include 간소화)

--- src/main/java/todolist/old_front/config/WebConfig.java
+++ src/main/java/todolist/old_front/config/WebConfig.java
@@ -20,29 +20,20 @@ public class WebConfig implements WebMvcConfigurer{
         List<String> excludeUrlApi = Arrays.asList("/api/v1/auth", 
                                                         "/api/v1/user/login", 
                                                         "/api/v1/user/join");
-        List<String> includeUrlApi = Arrays.asList("/api/v1/board/**", 
-                                                        "/api/v1/follow/**", 
-                                                        "/api/v1/user/{userId}");
         List<String> excludeUrlController = Arrays.asList("/user/login",
                                                                 "/user/join");
-        List<String> includeUrlController = Arrays.asList("/board/**",
-                                                                "/user/info");
         List<String> excludeUrlCommon = Arrays.asList("/css/**", 
                                                             "/error", 
                                                             "/*.ico");
-        List<String> includeUrlCommon = Arrays.asList();
 
         excludeUrlApi.addAll(excludeUrlController);
         excludeUrlApi.addAll(excludeUrlCommon);
-        includeUrlApi.addAll(includeUrlController);
-        includeUrlApi.addAll(includeUrlCommon);
 
         List<String> excludeUrl = excludeUrlApi;
-        List<String> includeUrl = excludeUrlApi;
         
         registry.addInterceptor(new LoginCheckInterceptor())    // 1. 인터셉터 등록
                 .order(1)                                 // 2. 인터셉터 체인 순서 지정
-                .addPathPatterns(excludeUrl)                    // 3. 인터셉터를 적용할 URL 패턴 설정
-                .excludePathPatterns(includeUrl);               // 4. 인터셉터에서 제외할 패턴 지정
+                .addPathPatterns("/**")             // 3. 전체에 적용시키고
+                .excludePathPatterns(excludeUrl);               // 4. 인터셉터에서 제외할 패턴 지정
     }
 }
